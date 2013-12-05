4eaec231bfcbb3b2c6c08cbd27369b2f0db576da
Ryan Singley <rsingley@gmail.com>
2013-12-05 14:11:02 -0500

    New branch for GUI

--- Roggle/src/RoggleView.java
+++ Roggle/src/RoggleView.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
 
-public interface roggleView {
+public interface RoggleView {
 	void messageToPlayer(String word);
 	void update(ArrayList<String> playerWords,int playerScore,String board,int cpuScore,ArrayList<String> cpuWords);
 	void newGame();
